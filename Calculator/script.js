d0f4dd8bd6df2159049e2ed5d1517636e1927a9d
Sejal Baviskar <sejalbaviskar5@gmail.com>
2026-06-20 10:45:18 +0530

    Initial commit

---
+++ Calculator/script.js
@@ -0,0 +1,28 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+let expression = "";
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        let value = this.textContent;
+
+        if (value === "C") {
+            expression = "";
+            display.value = "";
+        }
+        else if (value === "=") {
+            try {
+                expression = eval(expression);
+                display.value = expression;
+            } catch {
+                display.value = "Error";
+                expression = "";
+            }
+        }
+        else {
+            expression += value;
+            display.value = expression;
+        }
+    });
+}
